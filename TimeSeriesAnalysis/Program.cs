b2695b2b6c4a608bd2a9d9de35cbee27e071aa28
Steinar Elgsæter <stelg@equinor.com>
2021-01-25 16:53:17 +0100

    minor changes

---
+++ TimeSeriesAnalysis/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TimeSeriesAnalysis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
