2868db716f5a282f6b3ce3fd5ac858efb8383694
Steinar Elgsæter <STELG@equinor.com>
2021-02-01 14:55:32 +0100

    added ToString extension method for vectors

---
+++ Vector/VecExtensionMethods.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeSeriesAnalysis
+{
+    public static class VecExtensionMethods
+    {
+        public static string ToString(this double[] array,int nSignificantDigits)
+        {
+            StringBuilder sb = new StringBuilder();
+            if (array.Length > 0)
+            {
+                sb.Append("[");
+                sb.Append(SignificantDigits.Format(array[0], nSignificantDigits));
+                for (int i = 1; i < array.Length; i++)
+                {
+                    sb.Append(";");
+                    sb.Append(SignificantDigits.Format(array[i], nSignificantDigits) );
+                }
+                sb.Append("]");
+            }
+            return sb.ToString();
+        }
+    }
+}
