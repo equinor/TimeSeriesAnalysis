778a75160d522750dff8a3c33e19cc36754f9712
Steinar Elgsæter <STELG@equinor.com>
2021-02-03 08:45:42 +0100

    fix commas in ToString ","->"."

--- Vector/VecExtensionMethods.cs
+++ Vector/VecExtensionMethods.cs
@@ -3,22 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Globalization;
 
 namespace TimeSeriesAnalysis
 {
     public static class VecExtensionMethods
     {
-        public static string ToString(this double[] array,int nSignificantDigits)
+        public static string ToString(this double[] array,int nSignificantDigits,string dividerStr =";")
         {
             StringBuilder sb = new StringBuilder();
             if (array.Length > 0)
             {
                 sb.Append("[");
-                sb.Append(SignificantDigits.Format(array[0], nSignificantDigits));
+                sb.Append(SignificantDigits.Format(array[0], nSignificantDigits).ToString("", CultureInfo.InvariantCulture));
                 for (int i = 1; i < array.Length; i++)
                 {
-                    sb.Append(";");
-                    sb.Append(SignificantDigits.Format(array[i], nSignificantDigits) );
+                    sb.Append(dividerStr);
+                    sb.Append(SignificantDigits.Format(array[i], nSignificantDigits).ToString("",CultureInfo.InvariantCulture) );
                 }
                 sb.Append("]");
             }
