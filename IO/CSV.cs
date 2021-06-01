891e9cf2e6c98eb8a0fd78cf2f1e1bde75340854
Steinar Elgsæter <STELG@equinor.com>
2021-06-01 08:36:02 +0200

    load csv file code fix to use "using"

--- IO/CSV.cs
+++ IO/CSV.cs
@@ -9,56 +9,59 @@ namespace TimeSeriesAnalysis
 {
     public class CSV
     {
-        public static bool loadDataFromCSV(string filePath, out double[,] doubleData, out string[] variableNames, out string[,] stringData)
+        public static bool LoadDataFromCSV(string filePath, out double[,] doubleData, out string[] variableNames, out string[,] stringData)
         {
-            System.IO.StreamReader sr;
-            doubleData = null;
-            stringData = null;
-            variableNames = null;
-            try
+            using (System.IO.StreamReader sr = new System.IO.StreamReader(filePath))
             {
-                sr = new System.IO.StreamReader(filePath);
-            }
-            catch
-            {
-                return false;
-            }
-            var linesDouble = new List<double[]>();
-            var linesStr = new List<string[]>();
-            // readheader
-            variableNames = sr.ReadLine().Split(';');
-            while (!sr.EndOfStream)
-            {
-                double[] LineDouble = new double[variableNames.Length];
-                string currentLine = sr.ReadLine();
-                string[] LineStr = currentLine.Split(';');
-                for (int k = 0; k < LineStr.Length; k++)
+
+                doubleData = null;
+                stringData = null;
+                variableNames = null;
+               /* try
                 {
-                    if (LineStr[k].Length > 0)
-                        RobustParseDouble(LineStr[k], out LineDouble[k]);
-                    //   LineD[k] = double.Parse(Line[k], System.Globalization.CultureInfo.InvariantCulture);
+                    sr = new System.IO.StreamReader(filePath);
                 }
-                linesDouble.Add(LineDouble);
-                linesStr.Add(LineStr);
-            }
-            if (linesDouble.Count() > 0)
-            {
-                doubleData = new double[linesDouble.Count, linesDouble[0].Count()];
-            }
-            if (linesStr.Count() > 0)
-            {
-                stringData = new string[linesDouble.Count, linesStr[0].Count()];
-            }
-            for (int k = 0; k < linesDouble.Count; k++)
-                for (int l = 0; l < linesDouble[k].Count(); l++)
+                catch
                 {
-                    doubleData[k, l] = linesDouble[k][l];
-                }
-            for (int k = 0; k < linesStr.Count; k++)
-                for (int l = 0; l < linesStr[k].Count(); l++)
+                    return false;
+                }*/
+                var linesDouble = new List<double[]>();
+                var linesStr = new List<string[]>();
+                // readheader
+                variableNames = sr.ReadLine().Split(';');
+                while (!sr.EndOfStream)
                 {
-                    stringData[k, l] = linesStr[k][l];
+                    double[] LineDouble = new double[variableNames.Length];
+                    string currentLine = sr.ReadLine();
+                    string[] LineStr = currentLine.Split(';');
+                    for (int k = 0; k < LineStr.Length; k++)
+                    {
+                        if (LineStr[k].Length > 0)
+                            RobustParseDouble(LineStr[k], out LineDouble[k]);
+                        //   LineD[k] = double.Parse(Line[k], System.Globalization.CultureInfo.InvariantCulture);
+                    }
+                    linesDouble.Add(LineDouble);
+                    linesStr.Add(LineStr);
                 }
+                if (linesDouble.Count() > 0)
+                {
+                    doubleData = new double[linesDouble.Count, linesDouble[0].Count()];
+                }
+                if (linesStr.Count() > 0)
+                {
+                    stringData = new string[linesDouble.Count, linesStr[0].Count()];
+                }
+                for (int k = 0; k < linesDouble.Count; k++)
+                    for (int l = 0; l < linesDouble[k].Count(); l++)
+                    {
+                        doubleData[k, l] = linesDouble[k][l];
+                    }
+                for (int k = 0; k < linesStr.Count; k++)
+                    for (int l = 0; l < linesStr[k].Count(); l++)
+                    {
+                        stringData[k, l] = linesStr[k][l];
+                    }
+            }
             return true;
         }
 
