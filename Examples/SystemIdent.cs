a22a7e54d3d58e81775d314c56b8393cb39ce4d0
Steinar Elgsæter <steinelg@gmail.com>
2022-02-04 13:38:51 +0100

    - improvements to nonlinear identification example

--- Examples/SystemIdent.cs
+++ Examples/SystemIdent.cs
@@ -26,18 +26,34 @@ namespace TimeSeriesAnalysis._Examples
         #region ex_NONLINEAR_UNIT_MODEL
         public void NonlinearUnitModel()
         {
+            // constants of the designed 
             double timeConstant_s = 10;
             double timeDelay_s = 4;
-
             double timeBase_s = 1;
             double bias = 1;
             double noiseAmplitude = 0.1;
 
-            // create the input as a matrix
+            // create the input U as a matrix
             double[] u1 = TimeSeriesCreator.ThreeSteps(60, 120, 180, 240, 0, 1, 2, 3);
             double[] u2 = TimeSeriesCreator.ThreeSteps(90, 150, 210, 240, 2, 1, 3, 2);
             double[,] U = Array2D<double>.CreateFromList(new List<double[]> { u1, u2 });
 
+            // simulate a linear reference model for comparison
+            UnitParameters paramtersNoCurvature = new UnitParameters
+            {
+                WasAbleToIdentify = true,
+                TimeConstant_s = timeConstant_s,
+                TimeDelay_s = timeDelay_s,
+                LinearGains = new double[] { 2, -0.05 },
+                U0 = new double[] { 1.1, 1.1 },
+                Bias = bias
+            };
+            var refModel = new UnitModel(paramtersNoCurvature, timeBase_s, "Reference");
+            var refSim  = new UnitSimulator(refModel);
+            var refData = new UnitDataSet(timeBase_s,U);
+            refSim.Simulate(ref refData);
+
+            // simulate the nonlinear model 
             UnitParameters designParameters = new UnitParameters
             {
                 WasAbleToIdentify = true,
@@ -49,24 +65,6 @@ namespace TimeSeriesAnalysis._Examples
                 UNorm = new double[] { 1, 1 },// set this to make results comparable
                 Bias = bias
             };
-
-            UnitParameters paramtersNoCurvature = new UnitParameters
-            {
-                WasAbleToIdentify = true,
-                TimeConstant_s = timeConstant_s,
-                TimeDelay_s = timeDelay_s,
-                LinearGains = new double[] { 1, 0.7 },
-                U0 = new double[] { 1.1, 1.1 },
-                Bias = bias
-            };
-
-            // simulate a linear reference model for comparison
-            var refModel = new UnitModel(paramtersNoCurvature, timeBase_s, "Reference");
-            var refSim  = new UnitSimulator(refModel);
-            var refData = new UnitDataSet(timeBase_s,U);
-            refSim.Simulate(ref refData);
-
-            // simulate the nonlinear model 
             var trueModel = new UnitModel(designParameters, timeBase_s, "NonlinearModel1");
             var sim = new UnitSimulator(trueModel);
             var idDataSet = new UnitDataSet(timeBase_s,U);
--- articles/sysid_ex_nonlin.md
+++ articles/sysid_ex_nonlin.md
@@ -1,12 +1,18 @@
 
+This example shows how the ``UnitIdentifier`` can be used to identify a model that
+can include nonlinearities, time-delay and time-constant simultaneously.
 
-
+The code for this example:
 [!code-csharp[Examples](../Examples/SystemIdent.cs?name=ex_NONLINEAR_UNIT_MODEL)]
 
-The resulting figure
+It can be hard to see with the naked eye that a combination of inputs (U) and output (y) 
+are related non-linearly to another. For this reason, a reference linear model is also 
+simulated and plotted for comparison purposes. The significant difference between 
+``ymeas`` and ``yref(linear)`` in the below plot illustrate that the process is significantly non-linear. 
+
+The resulting figure, containing the dataset, the fitted model and the reference linear model. 
 
 ![Example 6](images/sysid_ex_nonlin1.png)
-
 
 The output of ``idModel.ToString()`` gives details on the model:
 ```
@@ -28,4 +34,28 @@ ProcessGains(at u0) :
  -> uNorm : [1;1]
 Bias : 0,997 ± 1,3
 ```
+Results: 
+- the estimated bounds of the estimated time-constant contains the actual time-constant(10), 
+- the estimated time delay matches the actual value (4),
+- the estimated bound of linear gains contain the actual values ``[1,0.7]``
+- the estimated bound of curvatures contain the actual values ``[1,-0.8]``
+- the estimated bias is close to the actual value (1).
 
+Overall, this is example shows a successful use of identification method. Under the hood 
+``UnitIdenfier`` has *automatically* tested and evaluated a number of different linear/nonlinear and static/dynamic 
+combinations before deciding on the returned nonlinear, dynamic model structure. 
+**The major intended design benefit of using ``UnitIdentifier`` is that the user is spared
+the details of this low-level model selection process.**
+
+Notice that this dataset had *three* step changes for each of the two inputs, to ensure
+that the curvatures were identifiable. In cases with less excitation in the dataset, 
+identification could have returned a local linear model, even though the underlying process
+was nonlinear. This is an unavoidable consequence of lack of excitation in the dataset, that one
+has to be aware of.  
+
+The user did *not* need to *specify* if the model should be linear or nonlinear, dynamic or static
+ or if the model should have time-delay or not. ``UnitIdentifier`` does need a 
+ ``UnitDataSet`` as input, thus the inputs(``U``) and output(``y``) need to be specified.
+*Thus ``UnitIdentifier`` is not be able to take a general ``TimeSeriesDataSet`` that contain 
+any number of un-ordered time-series and produce a general ``PlantSimulator``. 
+This more challenging task will be a topic for later examples.* 
