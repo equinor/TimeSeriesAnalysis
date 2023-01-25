12caf84f450e17b7ea2ad5e260fd996445cb8628
Steinar Elgsæter <steinelg@gmail.com>
2023-01-25 10:23:13 +0100

    added ordering to correlationcalculator

--- TimeSeriesAnalysis.Tests/Tests/CorrelationCalculator.cs
+++ TimeSeriesAnalysis.Tests/Tests/CorrelationCalculator.cs
@@ -25,13 +25,13 @@ namespace TimeSeriesAnalysis.Test
 
             var amplitude = 10;
 
-            var mainSignal = TimeSeriesCreator.Step(N/2,N,0, amplitude);
-            var oppositeSignal = TimeSeriesCreator.Step(N/2,N, amplitude, 0);
+            var mainSignal = TimeSeriesCreator.Step(N / 2, N, 0, amplitude);
+            var oppositeSignal = TimeSeriesCreator.Step(N / 2, N, amplitude, 0);
 
-            dataset.Add("mainSignal",mainSignal);
+            dataset.Add("mainSignal", mainSignal);
             dataset.Add("oppsiteSignal", oppositeSignal);
 
-            var results = CorrelationCalculator.Calculate("mainSignal",dataset);
+            var results = CorrelationCalculator.Calculate("mainSignal", dataset);
 
             Assert.AreEqual(-1, results["oppsiteSignal"]);
         }
@@ -64,7 +64,7 @@ namespace TimeSeriesAnalysis.Test
             var amplitude = 10;
 
             var mainSignal = TimeSeriesCreator.Step(N / 2, N, 0, amplitude);
-            var otherSignal = TimeSeriesCreator.Constant(0,N); 
+            var otherSignal = TimeSeriesCreator.Constant(0, N);
 
             dataset.Add("mainSignal", mainSignal);
             dataset.Add("oppsiteSignal", otherSignal);
@@ -72,6 +72,39 @@ namespace TimeSeriesAnalysis.Test
             var results = CorrelationCalculator.Calculate("mainSignal", dataset);
 
             Assert.AreEqual(0, results["oppsiteSignal"]);
+        }
+
+        [TestCase]
+        public void CorrelateAndOrder()
+        {
+            double timeBase_s = 1;
+
+            int N = 100;
+
+            var dataset = new TimeSeriesDataSet();
+
+            var amplitude = 10;
+
+            var mainSignal = TimeSeriesCreator.Step(N / 2, N, 0, amplitude);
+            var zeroSignal = TimeSeriesCreator.Constant(0, N);
+            var oppsiteSignal = TimeSeriesCreator.Step(N *7/15, N, amplitude, 0);  // "almost" correlated
+
+            dataset.Add("mainSignal", mainSignal);
+            dataset.Add("zeroSignal", zeroSignal);
+            dataset.Add("oppsiteSignal", oppsiteSignal);
+
+            var results = CorrelationCalculator.CalculateAndOrder("mainSignal", dataset);
+
+            Assert.AreEqual("mainSignal", results[0].signalName);
+            Assert.AreEqual(1, results[0].correlationFactor);
+
+            Assert.AreEqual("oppsiteSignal", results[1].signalName);
+            Assert.IsTrue(results[1].correlationFactor<-0.9);
+
+            Assert.AreEqual("zeroSignal", results[2].signalName);
+            Assert.AreEqual(0, results[2].correlationFactor);
+
+
         }
 
 
--- TimeSeriesAnalysis/CorrelationCalculator.cs
+++ TimeSeriesAnalysis/CorrelationCalculator.cs
@@ -6,6 +6,19 @@ using System.Text;
 
 namespace TimeSeriesAnalysis
 {
+    public class CorrelationObject
+    {
+        public string signalName;
+        public double correlationFactor;
+
+        public CorrelationObject(string name,double value)
+        {
+            signalName = name;
+            correlationFactor = value;
+        }
+    }
+
+
     public class CorrelationCalculator
     {
         /// <summary>
@@ -35,5 +48,39 @@ namespace TimeSeriesAnalysis
             }
             return returnCorrs;
         }
+
+        /// <summary>
+        /// Calculates correlation factors [-1,1] for a signal against all other signals in the dataset
+        /// returning the results in a list from highest to lowest score _absolute_ correlation factor
+        /// </summary>
+        /// <param name="signalName"></param>
+        /// <param name="dataSet"></param>
+        /// <returns>a</returns>
+        public static List<CorrelationObject> CalculateAndOrder(string signalName, TimeSeriesDataSet dataSet)
+        {
+            var mainSignal = dataSet.GetValues(signalName);
+
+            (double[,] matrix, string[] signalNames) = dataSet.GetAsMatrix();
+            double[,] corrMatrix = Measures.Correlation(matrix);
+            double[] corr = corrMatrix.GetColumn(0);
+
+            double[] sortedValues = Vec<double>.Sort((new Vec()).Abs(corr), VectorSortType.Descending,out int[] sortIdx);
+
+            List<CorrelationObject> ret = new List<CorrelationObject>();
+
+            for (int i=0; i < sortedValues.Length; i++)
+            {
+                string name = signalNames[sortIdx[i]];
+                double value = corr[sortIdx[i]];
+
+                ret.Add(new CorrelationObject(name, value)); 
+            
+            }
+            return ret;
+        }
+
+
+
+
     }
 }
