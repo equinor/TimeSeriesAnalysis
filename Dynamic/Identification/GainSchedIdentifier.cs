ddd92c94ba0f0c304b74d1b585e4fa6cf8d250ac
Steinar Elgsæter <stelg@equinor.com>
2024-01-31 14:56:51 +0100

    refactoring gainschedidentfier

--- Dynamic/Identification/GainSchedIdentifier.cs
+++ Dynamic/Identification/GainSchedIdentifier.cs
@@ -46,96 +46,70 @@ namespace TimeSeriesAnalysis.Dynamic
             GSp1.LinearGainThresholds = GS_LinearGainThreshold1;
 
             // Unit identifiers
-            var ui_1_1 = new UnitIdentifier();
-            FittingSpecs fittingSpecs_1g = new FittingSpecs();
-            double[] u_min_fit_1g = new double[number_of_inputs];
-            double[] u_max_fit_1g = new double[number_of_inputs];
-            for (int idx = 0; idx < number_of_inputs; idx++)
             {
-                if (idx == 0)
+                var ui_1_1 = new UnitIdentifier();
+                FittingSpecs fittingSpecs_1g = new FittingSpecs();
+                double[] u_min_fit_1g = new double[number_of_inputs];
+                double[] u_max_fit_1g = new double[number_of_inputs];
+                for (int idx = 0; idx < number_of_inputs; idx++)
                 {
-                    u_min_fit_1g[idx] = min_u;
-                    u_max_fit_1g[idx] = max_u;
+                    if (idx == 0)
+                    {
+                        u_min_fit_1g[idx] = min_u;
+                        u_max_fit_1g[idx] = max_u;
+                    }
+                    else
+                    {
+                        u_min_fit_1g[idx] = double.NaN;
+                        u_max_fit_1g[idx] = double.NaN;
+                    }
                 }
-                else
+                fittingSpecs_1g.U_min_fit = u_min_fit_1g;
+                fittingSpecs_1g.U_max_fit = u_max_fit_1g;
+                UnitModel UM1 = ui_1_1.IdentifyLinear(ref DS1, fittingSpecs_1g, false);
+                UnitParameters UMp1 = UM1.GetModelParameters();
+
+                // Simulate
+              /* double[] y_ref = dataSet.Y_meas;
+                int timeBase_s = 1;
+                var plantSim = new PlantSimulator(new List<ISimulatableModel> { UM1 });
+                var inputData = new TimeSeriesDataSet();
+                for (int k = 0; k < number_of_inputs; k++)
                 {
-                    u_min_fit_1g[idx] = double.NaN;
-                    u_max_fit_1g[idx] = double.NaN;
+                    inputData.Add(plantSim.AddExternalSignal(UM1, SignalType.External_U, (int)INDEX.FIRST), dataSet.U.GetColumn(k));
                 }
+                inputData.CreateTimestamps(timeBase_s);
+                var CorrectisSimulatable = plantSim.Simulate(inputData, out TimeSeriesDataSet simData);
+                double[] simY1 = simData.GetValues(UM1.GetID(), SignalType.Output_Y);*/
+                // Plot here!
+
+                //Shared.EnablePlots();
+                //Plot.FromList(new List<double[]> {
+                //    simY1,
+                //    y_ref,
+                //    dataSet.U.GetColumn(0) },
+                //    new List<string> { "y1=simY1_UM1", "y1=y_ref", "y3=u1" },
+                //timeBase_s,
+                //    "GainSched split ");
+                //Shared.DisablePlots();
+
+                // Linear gains
+                List<double[]> GS_LinearGains1 = new List<double[]>();
+                GS_LinearGains1.Add(UMp1.LinearGains);
+                GSp1.LinearGains = GS_LinearGains1;
+
+                // Time constants
+                double[] GS_TimeConstants_s1 = new double[] {UMp1.TimeConstant_s };
+                GSp1.TimeConstant_s = GS_TimeConstants_s1;
+
+                // Time constants thresholds
+                double[] GS_TimeConstantThreshold1 = new double[] { }; 
+                GSp1.TimeConstantThresholds = GS_TimeConstantThreshold1;
+                GSp1.Fitting = UMp1.Fitting;
+                double GSp1_rsqdiff_points = UMp1.Fitting.RsqDiff;
             }
-            fittingSpecs_1g.U_min_fit = u_min_fit_1g;
-            fittingSpecs_1g.U_max_fit = u_max_fit_1g;
-            UnitModel UM1 = ui_1_1.IdentifyLinear(ref DS1, fittingSpecs_1g,false);
-            UnitParameters UMp1 = UM1.GetModelParameters();
-
-            // Simulate
-            double[] y_ref = dataSet.Y_meas;
-            int timeBase_s = 1;
-            var plantSim = new PlantSimulator(new List<ISimulatableModel> { UM1 });
-            var inputData = new TimeSeriesDataSet();
-            for (int k = 0; k < number_of_inputs; k++)
-            {
-                inputData.Add(plantSim.AddExternalSignal(UM1, SignalType.External_U, (int)INDEX.FIRST), dataSet.U.GetColumn(k));
-            }
-            inputData.CreateTimestamps(timeBase_s);
-            var CorrectisSimulatable = plantSim.Simulate(inputData, out TimeSeriesDataSet simData);
-            double[] simY1 = simData.GetValues(UM1.GetID(), SignalType.Output_Y);
-
-            // Plot here!
-
-            //Shared.EnablePlots();
-            //Plot.FromList(new List<double[]> {
-            //    simY1,
-            //    y_ref,
-            //    dataSet.U.GetColumn(0) },
-            //    new List<string> { "y1=simY1_UM1", "y1=y_ref", "y3=u1" },
-            //timeBase_s,
-            //    "GainSched split ");
-            //Shared.DisablePlots();
-
-
-            // Linear gains
-            List<double[]> GS_LinearGains1 = new List<double[]>();
-            GS_LinearGains1.Add(UMp1.LinearGains);
-            GSp1.LinearGains = GS_LinearGains1;
-
-            // Time constants
-            double[] GS_TimeConstants_s1 = new double[] {UMp1.TimeConstant_s };
-            GSp1.TimeConstant_s = GS_TimeConstants_s1;
-
-            // Time constants thresholds
-            double[] GS_TimeConstantThreshold1 = new double[] { }; 
-            GSp1.TimeConstantThresholds = GS_TimeConstantThreshold1;
-            GSp1.Fitting = UMp1.Fitting;
-            double GSp1_rsqdiff_points = UMp1.Fitting.RsqDiff;
-
-            //GainSchedModel GSM1 = new GainSchedModel(GSp1, "my simple model");
-
-            //var plantSim1 = new PlantSimulator(new List<ISimulatableModel> { GSM1 });
-            //var inputData1 = new TimeSeriesDataSet();
-            //for (int k = 0; k < number_of_inputs; k++)
-            //{
-            //    inputData1.Add(plantSim.AddExternalSignal(GSM1, SignalType.External_U, (int)INDEX.FIRST), dataSet.U.GetColumn(k));
-            //}
-            //inputData1.CreateTimestamps(timeBase_s);
-            //var CorrectisSimulatable1 = plantSim1.Simulate(inputData1, out TimeSeriesDataSet simData1);
-            //double[] simY11 = simData1.GetValues(GSM1.GetID(), SignalType.Output_Y);
-
-            //// Plot here!
-
-            //Shared.EnablePlots();
-            //Plot.FromList(new List<double[]> {
-            //    simY11,
-            //    y_ref,
-            //    dataSet.U.GetColumn(0) },
-            //    new List<string> { "y1=simY1_GSM1", "y1=y_ref", "y3=u1" },
-            //timeBase_s,
-            //    "GainSched split ");
-            //Shared.DisablePlots();
-
-
+            ////////////////////////////////////////////////////
             // ## 2gain 1 time constant ##
-
             double[] potential_gainthresholds = new double[MAX_NUMBER_OF_THRESHOLDS]; 
             int m = (int)MAX_NUMBER_OF_THRESHOLDS/2;
 
@@ -176,21 +150,14 @@ namespace TimeSeriesAnalysis.Dynamic
 
                 List<double[]> GS_LinearGains2 = new List<double[]>();
                 double[] GS_TimeConstants_s2 = new double[2];
-                double[] GS_TimeConstantThreshold2 = new double[1]; // TODO: Should be considered to remove
-
-                // TODO: Should add a try - except block on this block
                 UnitModel UM2_a = ui_2g_1t_a.IdentifyLinear(ref DS2, fittingSpecs_a, false); ;
                 UnitParameters UMp2_a = UM2_a.GetModelParameters();
-      
-                // Linear gains
                 GS_LinearGains2.Add(UMp2_a.LinearGains);
-
-                // Time constants
                 GS_TimeConstants_s2[0] = UMp2_a.TimeConstant_s;
 
                 // b)
                 var ui_2g_1t_b = new UnitIdentifier();
-                FittingSpecs fittingSpecs_b = new FittingSpecs();
+        
                 double[] u_min_fit_b = new double[number_of_inputs];
                 double[] u_max_fit_b = new double[number_of_inputs];
                 for (int idx = 0; idx < number_of_inputs; idx++)
@@ -206,6 +173,7 @@ namespace TimeSeriesAnalysis.Dynamic
                         u_max_fit_b[idx] = double.NaN;
                     }
                 }
+                FittingSpecs fittingSpecs_b = new FittingSpecs();
                 fittingSpecs_b.U_min_fit = u_min_fit_b;
                 fittingSpecs_b.U_max_fit = u_max_fit_b;
                 fittingSpecs_b.u0 = new double[] { u_min_fit_b[0] + (u_max_fit_b[0] - u_min_fit_b[0])/2 };
@@ -277,18 +245,18 @@ namespace TimeSeriesAnalysis.Dynamic
                 var GSp = allGainSchedParams[i];
                 GainSchedModel GSM = new GainSchedModel(GSp, i.ToString());
 
-                plantSim = new PlantSimulator(new List<ISimulatableModel> { GSM });
-                inputData = new TimeSeriesDataSet();
+                var plantSim = new PlantSimulator(new List<ISimulatableModel> { GSM });
+                var  inputData = new TimeSeriesDataSet();
                 for (int k = 0; k < number_of_inputs; k++)
                 {
                     inputData.Add(plantSim.AddExternalSignal(GSM, SignalType.External_U, (int)INDEX.FIRST), dataSet.U.GetColumn(k));
                 }
-                inputData.CreateTimestamps(timeBase_s);
-                CorrectisSimulatable = plantSim.Simulate(inputData, out simData);
+                inputData.CreateTimestamps(dataSet.GetTimeBase());
+                var CorrectisSimulatable = plantSim.Simulate(inputData, out var simData);
                 if (CorrectisSimulatable)
                 {
-                    simY1 = simData.GetValues(GSM.GetID(), SignalType.Output_Y);
-                    var diff = vec.Subtract(simY1, y_ref);
+                    var simY1 = simData.GetValues(GSM.GetID(), SignalType.Output_Y);
+                    var diff = vec.Subtract(simY1, dataSet.Y_meas);
                     double rms = 0;
                     for (int j = 0; j < simY1.Length; j++)
                     {
@@ -317,6 +285,8 @@ namespace TimeSeriesAnalysis.Dynamic
                     }
                 }
             }
+
+
             return BestGainSchedParams;
         }
         
