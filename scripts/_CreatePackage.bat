2cd165c0892af56e8a912a6b7a7606fa814c671b
Steinar Elgsæter <stelg@equinor.com>
2024-10-31 09:00:01 +0100

    Revert "tried to debug #142, but to no ovail"
    This reverts commit 801d6941a46b1ba5392896d693b85498a3ef0962.

--- scripts/_CreatePackage.bat
+++
@@ -1,4 +0,0 @@
-cd ..
-dotnet build TimeSeriesAnalysis.sln -c Release -p:NuGetBuild=True -p:GenerateDocumentationFile=True -p:DebugSymbols=true -p:DebugType=portable
-dotnet pack TimeSeriesAnalysis.sln -c Release  --include-symbols -p:NuGetBuild=True -p:SymbolPackageFormat=snupkg --include-source
-pause
