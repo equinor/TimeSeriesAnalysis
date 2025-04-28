fd6adb3efdd2ae9fcd92a10f40701cf3b38f7652
Steinar Elgsæter <stelg@equinor.com>
2025-04-28 15:13:24 +0200

    -fix issue that caused pididentifier to not build

--- Dynamic/Identification/PidIdentifier.cs
+++ Dynamic/Identification/PidIdentifier.cs
@@ -89,7 +89,7 @@ namespace TimeSeriesAnalysis.Dynamic
 
             // 2. try identification without delay of one sample (yields better results often if dataset is downsampled
             //    relative to the clock that the pid algorithm ran on originally)
-            (PidParameters results_withoutDelay, double[,] U_withoutDelay) = IdentifyInternal(dataSet, false, ignoreFlatLines: ignoreFlatLinesFirst);
+            (PidParameters results_withoutDelay, double[,] U_withoutDelay) = IdentifyInternal(dataSet, false, ignoreFlatLines: ignoreFlatLines);
 
             // save which is the "best" estimate for comparison 
             bool doDelay = true;
@@ -117,7 +117,7 @@ namespace TimeSeriesAnalysis.Dynamic
                 for (double filterTime_s = timeBase_s; filterTime_s < maxFilterTime_s; filterTime_s += timeBase_s)
                 {
                     var pidFilterParams = new PidFilterParams(true, 1, filterTime_s);
-                    (PidParameters results_withFilter, double[,] U_withFilter) = IdentifyInternal(dataSet, doDelay, pidFilterParams, ignoreFlatLines: ignoreFlatLinesFirst);
+                    (PidParameters results_withFilter, double[,] U_withFilter) = IdentifyInternal(dataSet, doDelay, pidFilterParams, ignoreFlatLines);
 
                     if (IsFirstModelBetterThanSecondModel(results_withFilter, bestPidParameters))
                     {
@@ -135,7 +135,7 @@ namespace TimeSeriesAnalysis.Dynamic
                 {
                     var pidFilterParams = new PidFilterParams(true, 1, filterTime_s);
                     (PidParameters results_withFilter, double[,] U_withFilter) =
-                        IdentifyInternal(dataSet, doDelay, pidFilterParams, filterUmeas, ignoreFlatLines: ignoreFlatLinesFirst);
+                        IdentifyInternal(dataSet, doDelay, pidFilterParams, filterUmeas, ignoreFlatLines);
 
                     if (IsFirstModelBetterThanSecondModel(results_withFilter, bestPidParameters))
                     {
